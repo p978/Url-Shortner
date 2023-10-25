3c68daf04687e4ec221a892a674f12dc4976cf95
rcx69 <59633184+p978@users.noreply.github.com>
2023-10-25 11:36:27 +0545

    ....

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import string,random,models,writingsq
+import string,random
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 app = FastAPI()
 # Connect to my sql Database
-from database import *
-models.Base.metadata.create_all(bind=engine)
 class url_data(BaseModel):
     previous_url : str
 # The root
@@ -14,5 +13,7 @@ async def root():
 @app.post("/urls/")
 async def get_url(urls:url_data):
     res = ''.join(random.choices(string.ascii_letters, k=4))
-    writingsq.write(res)
+    @app.get("/"+res)
+    async def redirect_typer(u:url_data):
+        return u.previous_url()
     return {"Shorted url is": "http://127.0.0.1:8000/"+res}
--- writingsq.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-def write(code):
-    sqliteConnection = sqlite3.connect('urls.db')
-    cursor = sqliteConnection.cursor()
-    count = cursor.execute("INSERT INTO urls (key) VALUES(?)", (code,))
-    sqliteConnection.commit()
