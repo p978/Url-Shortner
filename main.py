0391a910bebe7bdec3dc835e5dd0da5c45e24e9f
rcx69 <59633184+p978@users.noreply.github.com>
2023-10-25 12:31:24 +0545

    last commit in the code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 app = FastAPI()
 # Connect to my sql Database
 class url_data(BaseModel):
-    previous_url : str
+    previous_url: str
 # The root
 @app.get("/")
 async def root():
@@ -14,6 +14,9 @@ async def root():
 async def get_url(urls:url_data):
     res = ''.join(random.choices(string.ascii_letters, k=4))
     @app.get("/"+res)
-    async def redirect_typer(u:url_data):
-        return u.previous_url()
-    return {"Shorted url is": "http://127.0.0.1:8000/"+res}
+    async def redirect_typer():
+        if "https://" and ".com" in urls.previous_url:
+            return RedirectResponse(urls.previous_url)
+        else:
+            return RedirectResponse("https://"+urls.previous_url+".com")
+    return {"Shorted url is": "http://127.0.0.1:8000/"+res} 
